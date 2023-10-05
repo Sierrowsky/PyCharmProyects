d9a4f7e9b47a4ac2561e339944f3f7a4932bb602
PabloSierra <13sierras@gmail.com>
2023-10-05 14:20:38 +0200

    Update

--- sierraperez/eventos.py
+++ sierraperez/eventos.py
@@ -5,8 +5,16 @@ class Eventos():
             sys.exit()
         except Exception as error:
             print(error,"en modulo eventos")
+
+    @staticmethod
     def abrirCalendar(self):
         try:
             var.calendar.show()
         except Exception as error:
             print("error al abrir calendario", error)
+    @staticmethod
+    def acercade(self):
+        try:
+            pass
+        except Exception as error:
+            print("Error abrir ventana acerca de ", error)

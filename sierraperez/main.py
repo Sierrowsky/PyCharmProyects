8a8255ff09f6e338d8ee7ec5e10d8f1b14a8bc18
PabloSierra <13sierras@gmail.com>
2023-10-16 12:22:51 +0200

    Update

--- sierraperez/main.py
+++ sierraperez/main.py
@@ -1,40 +1,8 @@
-import sys, eventos, var
-
-import drivers
-from Calendar import *
 from MainWindow import *
-from Salir import *
-from About import *
-from datetime import datetime
+from windowAux import *
 
 
-class Calendar(QtWidgets.QDialog):
-    def __init__(self):
-        super(Calendar, self).__init__()
-        var.calendar = Ui_digCalendar()
-        var.calendar.setupUi(self)
-        dia = datetime.now().day
-        mes = datetime.now().month
-        ano = datetime.now().year
-        var.calendar.Calendar.setSelectedDate((QtCore.QDate(ano, mes, dia)))
-        var.calendar.Calendar.clicked.connect(drivers.Drivers.CargaFecha)
 
-
-class Salir(QtWidgets.QDialog):
-    def __init__(self):
-        super(Salir, self).__init__()
-        var.salir = Ui_dlgSalir()
-        var.salir.setupUi(self)
-        var.salir.btnSalirAceptar.clicked.connect(eventos.Eventos.btnSalir)
-        var.salir.btnSalirCancelar.clicked.connect(eventos.Eventos.btnCancelar)
-
-
-class About(QtWidgets.QDialog):
-    def __init__(self):
-        super(About, self).__init__()
-        var.about = Ui_dlgAbout()
-        var.about.setupUi(self)
-        var.about.btnCerrraAbout.clicked.connect(eventos.Eventos.btnCerrarAbout)
 
 
 class Main(QtWidgets.QMainWindow):
